8372f058d454d5c8a403984614482fc9171c2c90
kubilay13 <145432835+kubilay13@users.noreply.github.com>
2024-10-25 23:29:08 +0300

    add Tronscan link

--- TronGenerateWallet/Services/TronWalletGenerateService.cs
+++ TronGenerateWallet/Services/TronWalletGenerateService.cs
@@ -16,8 +16,9 @@ namespace TronGenerateWallet.Services
             var key = TronECKey.GenerateKey(TronNetwork.MainNet);
             var privatekey=key.GetPrivateKey();
             var address = key.GetPublicAddress();
+            var tronScanLink = $"https://tronscan.org/#/address/{address}";
             Console.WriteLine($"--PrivateKey:{privatekey}\nWalletAdress:{address}--");
-            return $"Private Key:{privatekey}\nAddress:{address}";
+            return $"Private Key: {privatekey}\nAddress: {address}\nTronScan: {tronScanLink}";
         }
     }
 }
